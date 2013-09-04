c1f68d965e89af0f9c7c5087868b672a040719ec
liebeskind <daniel.liebeskind@gmail.com>
2013-09-04 14:24:26 -0700

    Added code for stringifyJSON. -checks whether obj is an array, string, number or object -if array, loop through each property of object and add each property to variable result within brackets separated by a comma by calling stringifyJSON(obj[i]) -if string, return the object in quotes -if number, return the object converted toString(); -if object, loop through the properties and call stringifyJSON(obj[prop]) to return and add elements correctly.  Show the property key if it is not a number. Join all of the results together with bracket and separate with comma. -return 'null' if obj is undefined -Still having trouble getting all of the tests to pass and have to figure out when to use [] vs {}.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,42 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
-var stringifyJSON = function (obj) {
-  // your code goes here
-};
+function stringifyJSON(obj) {
+		
+		if(obj != undefined){
+			if (typeof obj === 'array'){
+				var result = "[";
+				for(var i=0;i<obj.length;i++)
+				{
+					if(i>0) result += ",";
+					result += stringifyJSON(obj[i]);
+				}
+				result += "]"
+				return result;
+			} else if (typeof obj === 'string'){
+				return ('"' + obj + '"');
+			} else if (typeof obj === 'number'){
+				return obj.toString(); 		
+			} else if (typeof obj === 'object'){
+				var holder = [];
+				for (prop in obj){
+					if (typeof obj[prop] != 'function'){
+						if (!isNaN(prop)){
+							holder.push(stringifyJSON(obj[prop])); 
+						} else {
+							holder.push('"'+ prop + '":' + stringifyJSON(obj[prop]));
+						}
+					}
+				}
+				if (holder.length>0){
+				return "{" + holder.join(",") + "}";
+				} else {
+					return "{}"
+				}
+			} else {
+				return obj.toString();
+			}
+		} else {
+		return 'null';
+		}
+	};
